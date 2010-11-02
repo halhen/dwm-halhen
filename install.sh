259f55a1aa7022439c94730269a5e469cf437a34
Henrik Hallberg <halhen@k2h.se>
2010-11-02 12:57:10 +0100

    Automatically update version to what we're building from upstream

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+_version=$(sed -n 's/VERSION = \(.*\)/\1/p' src/config.mk)
+sed -i "s/pkgver=.*/pkgver=$_version/" PKGBUILD
+
+echo "Building version $_version"
+
 makepkg -f -i
 rm -rf pkg
 rm *.pkg.tar.xz
